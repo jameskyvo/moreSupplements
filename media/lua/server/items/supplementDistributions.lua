3b60cab0bce3433b99f28b45e804f79d25619b34
James Kelley <james.kelley.netdev@gmail.com>
2024-10-13 18:27:50 -0500

    Fixed issue #8: Inserted into loot distribution tables

---
+++ media/lua/server/items/supplementDistributions.lua
@@ -0,0 +1,24 @@
+require 'Items/ProceduralDistributions'
+
+-- ["Protein Powder"]
+table.insert(ProceduralDistributions["list"]["ArmyStorageMedical"].items, "JunkiezMoreSupplements.JunkiezProteinPowder");
+table.insert(ProceduralDistributions["list"]["ArmyStorageMedical"].items, 1);
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "JunkiezMoreSupplements.JunkiezProteinPowder");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 1.5);
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "JunkiezMoreSupplements.JunkiezProteinPowder");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 2);
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, "JunkiezMoreSupplements.JunkiezProteinPowder");
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, 2);
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, "JunkiezMoreSupplements.JunkiezProteinPowder");
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, 1);
+
+
+-- ["Protein Shake"]
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "JunkiezMoreSupplements.JunkiezProteinShake");
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 2);
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, "JunkiezMoreSupplements.JunkiezProteinShake");
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, 2);
+table.insert(ProceduralDistributions["list"]["FridgeSnacks"].items, "JunkiezMoreSupplements.JunkiezProteinShake");
+table.insert(ProceduralDistributions["list"]["FridgeSnacks"].items, 1);
+table.insert(ProceduralDistributions["list"]["FridgeWater"].items, "JunkiezMoreSupplements.JunkiezProteinShake");
+table.insert(ProceduralDistributions["list"]["FridgeWater"].items, 1);
