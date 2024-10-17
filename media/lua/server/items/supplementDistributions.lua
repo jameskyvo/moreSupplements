8ec603e72de88521c59405bf42410803d0209d2d
James Kelley <james.kelley.netdev@gmail.com>
2024-10-17 12:51:31 -0500

    Adds feature #13, mass gainer and fat burner

--- media/lua/server/items/supplementDistributions.lua
+++ media/lua/server/items/supplementDistributions.lua
@@ -22,8 +22,10 @@ table.insert(ProceduralDistributions["list"]["FridgeSnacks"].items, "JunkiezMore
 table.insert(ProceduralDistributions["list"]["FridgeSnacks"].items, 1);
 table.insert(ProceduralDistributions["list"]["FridgeWater"].items, "JunkiezMoreSupplements.JunkiezProteinShake");
 table.insert(ProceduralDistributions["list"]["FridgeWater"].items, 1);
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "JunkiezMoreSupplements.JunkiezProteinShake");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 1);
 
--- ["Preworkout Powder"]
+-- ["Preworkout"]
 table.insert(ProceduralDistributions["list"]["ArmyStorageMedical"].items, "JunkiezMoreSupplements.JunkiezPreworkout");
 table.insert(ProceduralDistributions["list"]["ArmyStorageMedical"].items, 0.25);
 table.insert(ProceduralDistributions["list"]["GymLockers"].items, "JunkiezMoreSupplements.JunkiezPreworkout");
@@ -34,3 +36,27 @@ table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, "JunkiezMo
 table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, 0.1);
 table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, "JunkiezMoreSupplements.JunkiezPreworkout");
 table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, 0.01);
+
+-- ["Fat Burner"]
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "JunkiezMoreSupplements.JunkiezFatBurner");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 1);
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "JunkiezMoreSupplements.JunkiezFatBurner");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 0.55);
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, "JunkiezMoreSupplements.JunkiezFatBurner");
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, 0.1);
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, "JunkiezMoreSupplements.JunkiezFatBurner");
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, "JunkiezMoreSupplements.JunkiezFatBurner");
+table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, 1);
+
+-- ["Mass Gainer"]
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "JunkiezMoreSupplements.JunkiezMassGainer");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 1);
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "JunkiezMoreSupplements.JunkiezMassGainer");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 0.25);
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, "JunkiezMoreSupplements.JunkiezMassGainer");
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, 0.1);
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, "JunkiezMoreSupplements.JunkiezMassGainer");
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, "JunkiezMoreSupplements.JunkiezMassGainer");
+table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, 1);
