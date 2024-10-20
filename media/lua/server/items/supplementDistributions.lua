681a8b92749ffbceea98f7cb12c9c22f17007977
James Kelley <james.kelley.netdev@gmail.com>
2024-10-20 00:13:46 -0500

    Added energy drink

--- media/lua/server/items/supplementDistributions.lua
+++ media/lua/server/items/supplementDistributions.lua
@@ -60,3 +60,27 @@ table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, "Junkie
 table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, 0.01);
 table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, "JunkiezMoreSupplements.JunkiezMassGainer");
 table.insert(ProceduralDistributions["list"]["PharmacyCosmetics"].items, 1);
+
+-- ["Energy Drink"]
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["BandPracticeFridge"].items, 1);
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["CafeteriaDrinks"].items, 0.1);
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, 0.25);
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["FridgeBreakRoom"].items, 0.25);
+table.insert(ProceduralDistributions["list"]["FridgeOffice"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["FridgeOffice"].items, 0.25);
+table.insert(ProceduralDistributions["list"]["FridgeSoda"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["FridgeSoda"].items, 0.10);
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 1);
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, 10);
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, 10);
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 2);
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "JunkiezMoreSupplements.JunkiezEnergyDrink");
+table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
