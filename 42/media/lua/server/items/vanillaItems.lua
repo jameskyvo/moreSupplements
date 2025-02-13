18faca4887a2013c717e5d98ccdae798531740f5
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2025-02-12 19:35:01 -0600

    Issue #39. Added temporary workaround
    Used Template: Salad to be able to add most fruits to the evolved recipe for blended shake. DoParam and other evolvedrecipe modifications are not currently working for B42.

--- 42/media/lua/server/items/vanillaItems.lua
+++ 42/media/lua/server/items/vanillaItems.lua
@@ -1,23 +1,32 @@
--- Sets a vanilla ingredient as a valid shake crafting item.
-function setBlenderShakeIngredient(itemName, hungerValue)
+-- Gamejunkiez: getEvolvedRecipe():add and DoParam do NOT work as of b42.3! This script is currently not working properly.Template:Salad in supplementevolvedrecipes is a workaround until fixed.
+
+-- Sets a vanilla ingredient as a valid shake crafting item for the blender bottle shake.
+function SetBlenderShakeIngredient(itemName, hungerValue)
 	local item = ScriptManager.instance:getItem("Base." .. itemName)
 	if item then
-		item:DoParam("EvolvedRecipe = JunkiezBlendedShake:"..hungerValue)
 		print("Adding " .. itemName .. " as an ingredient for a blended shake for " .. hungerValue .. " hunger.")
+		item:getEvolvedRecipe():add("JunkiezBlendedShake:" .. hungerValue)
+		print("Final EvolvedRecipe Parameters for: " .. itemName)
+		print(item:getEvolvedRecipe())
+	else
+		print("ERROR: SetBlenderShakeIngredient could not find item: " .. itemName)
 	end
 end
-
-function setShakeIngredient(itemName, hungerValue)
+-- Sets a vanilla ingredient as a valid shake crafting item for the plastic bottle shake.
+function SetShakeIngredient(itemName, hungerValue)
 	local item = ScriptManager.instance:getItem("Base." .. itemName)
 	if item then
-		item:DoParam("EvolvedRecipe = JunkiezShake:" .. hungerValue)
 		print("Adding " .. itemName .. " as an ingredient for a blended shake for " .. hungerValue .. " hunger.")
+		item:getEvolvedRecipe():add("JunkiezBlendedShake:" .. hungerValue)
+		print("Final EvolvedRecipe Parameters for: " .. itemName)
+		print(item:getEvolvedRecipe())
+	else
+		print("ERROR: SetShakeIngredient could not find item: " .. itemName)
 	end
 end
-
-
 
 -- Add ingredients for use in creating shakes
 
 -- Canned Foods
-setBlenderShakeIngredient("CannedCarrotsOpen",4)
+SetBlenderShakeIngredient("Apple",4)
+SetBlenderShakeIngredient("CannedCarrotsOpen",4)
