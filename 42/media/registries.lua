6d2cd5ed78d24d5dee3d436f4c204bac2ba27722
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2025-12-16 17:39:29 -0600

    Added Items to B42.13's tagging system

---
+++ 42/media/registries.lua
@@ -0,0 +1,14 @@
+-- Shakes
+ItemTag.register("junkiezmoresupplements:shake")
+ItemTag.register("junkiezmoresupplements:blendedshake")
+
+-- Powders
+ItemTag.register("junkiezmoresupplements:proteinpowder")
+ItemTag.register("junkiezmoresupplements:preworkout")
+ItemTag.register("junkiezmoresupplements:massgainer")
+ItemTag.register("junkiezmoresupplements:fatburner")
+
+-- Misc
+ItemTag.register("junkiezmoresupplements:energydrink")
+ItemTag.register("junkiezmoresupplements:proteinbar")
+ItemTag.register("junkiezmoresupplements:blenderbottle")
